ce03842a82e055232e68a49a1ded49621336ea43
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-03-25 13:12:36 -0700

    所有mysql字段中文名

--- database/customer.sql
+++ database/customer.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS `演示`.`顾客`;
 CREATE TABLE  `演示`.`顾客` (
   `顾客_ID` bigint(20) unsigned COLLATE utf8_unicode_ci NOT NULL AUTO_INCREMENT,
   `姓名` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `ADDRESS` varchar(255) NOT NULL,
-  `CREATED_DATE` datetime NOT NULL,
+  `地址` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `创建日期` datetime COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`顾客_ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
