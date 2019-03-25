337140d4561410f038db10e59172a97dae652823
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-03-25 11:30:52 -0700

    一个mysql中文字段, 添加和显示正确

--- database/customer.sql
+++ database/customer.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS `mkyong`.`customer`;
 CREATE TABLE  `mkyong`.`customer` (
   `CUSTOMER_ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `NAME` varchar(45) NOT NULL,
+  `姓名` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `ADDRESS` varchar(255) NOT NULL,
   `CREATED_DATE` datetime NOT NULL,
   PRIMARY KEY (`CUSTOMER_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
