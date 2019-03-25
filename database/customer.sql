8c10846a7faa7a920038edce2f513d60d8a01fa6
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-03-25 12:55:31 -0700

    中文mysql主键

--- database/customer.sql
+++ database/customer.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS `mkyong`.`顾客`;
 CREATE TABLE  `mkyong`.`顾客` (
-  `CUSTOMER_ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `顾客_ID` bigint(20) unsigned COLLATE utf8_unicode_ci NOT NULL AUTO_INCREMENT,
   `姓名` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `ADDRESS` varchar(255) NOT NULL,
   `CREATED_DATE` datetime NOT NULL,
-  PRIMARY KEY (`CUSTOMER_ID`)
+  PRIMARY KEY (`顾客_ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
